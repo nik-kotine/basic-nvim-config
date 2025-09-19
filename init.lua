c24c909666180e17f62ac42f447dde4a9869db5b
nik-kotine <mswv280407@gmail.com>
2025-09-18 23:00:10 -0500

    feat: autocompletion, courtesy of elgatohumilde

--- init.lua
+++ init.lua
@@ -6,6 +6,9 @@ vim.o.smartcase = true -- ignore case when search pattern is all lowercase
 vim.o.ignorecase = true -- ^ ditto
 vim.o.hlsearch = false -- clear search highlights after submit
 vim.opt.smartindent = true -- auto indentation
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.writebackup = false
 -- vim.o.signcolumn = 'yes' -- enables gutter next to line numbers for plugin icons
 
 -- theme
@@ -13,6 +16,15 @@ vim.cmd.colorscheme('habamax')
 
 -- leader key
 vim.g.mapleader = vim.keycode('<Space>')
+
+-- lazy plugins
+vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
+
+require("lazy").setup({
+	"nvim-lua/plenary.nvim",
+    "neovim/nvim-lspconfig",       -- Configuración de LSP
+	{"Saghen/blink.cmp",version = "*"}
+})
 
 -- personal configs: delete if needed
 vim.api.nvim_set_keymap("i", "\\{", "<", { noremap = true }) -- maps \{ to <
@@ -28,3 +40,8 @@ end, { desc = "Previous diagnostic" })
 vim.keymap.set("n", "ee", function() -- maps 'ee' for next diagnostic
   vim.diagnostic.goto_next({ severity = nil })
 end, { desc = "Next diagnostic" })
+
+require "blink-cmp".setup {
+	completion = {documentation = {auto_show = true}}
+}
+vim.lsp.enable({"clangd"})
